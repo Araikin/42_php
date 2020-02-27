f1e4c1230767cfd2da1633cbdbecd28a178c3e1f
Aray Sultanbekova <asultanb@e1z1r4p37.42.us.org>
2020-02-26 18:15:12 -0800

    doing ex01

--- d02/ex01/one_more_time.php
+++ d02/ex01/one_more_time.php
@@ -14,14 +14,29 @@ function process($arr) {
 		"9" => "Septembre",
 		"10" => "Octobre",
 		"11" => "Novembre",
-		"12" => "Decembre");
+        "12" => "Decembre");
+    $d = array (
+        "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
+    print_r($d);
     array_shift($arr);
+}
+
+function check($arr) {
+    if (preg_match("/^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $arr[4]) != 0 ||
+        preg_match("/^[0-9]{4}$/", $arr[3]) != 0)
+        return true;
+    return false;
+
+    // if (preg_match("", $arr[0]) != 0 || preg_match("", $arr[1]) != 0 ||
+    //     preg_match("", $arr[2]) != 0 || preg_match("/^[0-9]{4}$/", $arr[3]) != 0 ||
+    //     return false;
+
 }
 
 if ($argc == 2)
 {
     $arr = explode(" ", ucwords($argv[1]));
-    if (count($arr) == 5)
+    if (count($arr) == 5 && check($arr))
         process($arr);
     else
         echo "Wrong Format\n";
